068ed4df7c9c65ae10bc5c5d49f9a0eb6904ee33
Alvaro <alvarodr_97@hotmail.com>
2023-10-12 21:02:00 +0200

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
 import { ConfettiProvider } from '@/components/providers/confetti-provider'
 
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -24,6 +26,7 @@ export default function RootLayout({
           <ConfettiProvider />
           <ToastProvider />
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
